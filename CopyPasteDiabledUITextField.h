82a297620e5d2110091bcdc55ac5412bde747e01
satheeshwaran <satheesh4590@gmail.com>
2013-07-17 19:46:45 +0530

    Adding intital files

---
+++ CopyPasteDiabledUITextField.h
@@ -0,0 +1,13 @@
+//
+//  CopyPasteDiabledUITextField.h
+//  TextField
+//
+//  Created by Satheeshwaran on 7/17/13.
+//  Copyright (c) 2013 Cognizant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CopyPasteDiabledUITextField : UITextField
+
+@end
---
+++ CopyPasteDiabledUITextField.m
@@ -0,0 +1,40 @@
+//
+//  CopyPasteDiabledUITextField.m
+//  TextField
+//
+//  Created by Satheeshwaran on 7/17/13.
+//  Copyright (c) 2013 Cognizant. All rights reserved.
+//
+
+#import "CopyPasteDiabledUITextField.h"
+
+@implementation CopyPasteDiabledUITextField
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+
+-(BOOL)canPerformAction:(SEL)action withSender:(id)sender {
+    UIMenuController *menuController = [UIMenuController sharedMenuController];
+    if (menuController) {
+        [UIMenuController sharedMenuController].menuVisible = NO;
+    }
+    return NO;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
