43f7fa5ee485ba7acec98b5984ed3f02d5117ed2
satheeshwaran <satheesh4590@gmail.com>
2013-07-24 14:11:49 +0530

    Removed copyright stmts

--- CopyPasteDiabledUITextField.h
+++ CopyPasteDiabledUITextField.h
@@ -3,7 +3,7 @@
 //  TextField
 //
 //  Created by Satheeshwaran on 7/17/13.
-//  Copyright (c) 2013 Cognizant. All rights reserved.
+//  Copyright (c) 2013 Satheeshwaran. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
